461f297a0a37d64c46d4904add8f8831ccbac11a
ShawnellHarrison <150714990+ShawnellHarrison@users.noreply.github.com>
2023-12-16 12:09:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# employee-info-cards-main
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/ShawnellHarrison/employee-info-cards-main)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="card">
+      <img src="e.jpg" alt="Avatar" style={{ width: '100%' }} />
+      <div className="container">
+        <h4><b>John Doe</b></h4>
+        <p>Architect & Engineer</p>
+      </div>
+    </div>
+  );
+}
